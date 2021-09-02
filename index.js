1c650a7240ba62c2d51bcfe568d0ed91bf934bbb
robbiereta <robbie.reta67@gmail.com>
2021-09-02 15:04:46 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var http = require("http");
+
+//create a server object:
+http
+  .createServer(function (req, res) {
+    res.write("Hello World!"); //write a response to the client
+    res.end(); //end the response
+  })
+  .listen(8080); //the server object listens on port 8080
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
