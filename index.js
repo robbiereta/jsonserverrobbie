798c39237c607fe01e1f2c5649dac570c6268b07
robbiereta <robbie.reta67@gmail.com>
2021-09-02 15:11:16 -0500

    ready?

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-var http = require("http");
-
-//create a server object:
-http
-  .createServer(function (req, res) {
-    res.write("Hello World!"); //write a response to the client
-    res.end(); //end the response
-  })
-  .listen(8080); //the server object listens on port 8080
+//the server object listens on port 8080
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
